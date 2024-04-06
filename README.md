47f6a318d93fd74d786d457b1a7d3309fc3ef59b
itsnot-aduck <hoangduc1662002@gmail.com>
2024-04-06 23:12:18 +0700

    doc: edit readme.md

--- README.md
+++ README.md
@@ -17,3 +17,245 @@ At the end, user of the app should be able to
 ## Initial Instruction
 
 See [Instruction](https://github.com/itsnot-aduck/trivia-api/blob/main/INSTRUCTION.md) given by Udacity
+
+## Getting Started
+
+### Pre-requisites and Local Development
+
+Developers using this project should have Python3.7, pip and node Installed on the local machine
+
+#### Backend
+
+From the backend folder run `pip install requirements.txt`. All required packages are included in the requirements file.
+
+To run the application run the following commands:
+
+```
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run
+```
+
+These commands put the application in development and directs our application to use the `__init__.py` file in our flaskr folder. Working in development mode shows an interactive debugger in the console and restarts the server whenever changes are made. If running locally on Windows, look for the commands in the [Flask documentation](http://flask.pocoo.org/docs/1.0/tutorial/factory/).
+
+The application is run on `http://127.0.0.1:5000/` by default and is a proxy in the frontend configuration.
+
+#### Frontend
+
+From the frontend folder, run the following commands to start the client:
+
+```
+npm install // only once to install dependencies
+npm start
+```
+
+By default, the frontend will run on localhost:3000.
+
+### Tests
+
+In order to run tests navigate to the backend folder and run the following commands:
+
+```
+dropdb trivia_test
+createdb trivia_test
+psql trivia_test < trivia.psql
+python test_flaskr.py
+```
+
+The first time you run the tests, omit the dropdb command.
+
+All tests are kept in that file and should be maintained as updates are made to app functionality.
+
+## API Reference
+
+### Quick Start
+
+- Base URL: This app can locally run and is not hosted as a base URL. The backend app is hosted at the default `http://127.0.0.1:5000` or `localhost:5000`, which is set as a proxy in the frontend configuration
+
+- Authentication: This version doesn't require authentication or API keys
+
+### Error Handing
+
+An error can be returned as JSON Objects with the format:
+
+```
+{
+    "success": False,
+    "error": 400,
+    "message": "bad request"
+}
+```
+
+The API can return some error types when request fail
+
+- 400: Bad Request
+- 404: Resource Not Found
+- 422: Not Processable
+- 405: Method not allowed
+- 500: Internal server error
+
+### Endpoints
+
+#### `GET '/categories'`
+
+- Fetches a dictionary of categories in which the keys are the ids and the value is the corresponding string of the category
+- Request Arguments: None
+- Returns: An object with a single key, categories, that contains an object of id: category_string key:value pairs.
+
+Example: `curl -X POST -H "Content-Type: application/json" http://127.0.0.1:5000/categories`
+
+```
+{
+  "categories": {
+    "1": "Science",
+    "2": "Art",
+    "3": "Geography",
+    "4": "History",
+    "5": "Entertainment",
+    "6": "Sports"
+  }
+}
+```
+
+#### `GET '/questions?page=${integer}'`
+
+- Fetches a paginated set of questions, a total number of questions, all categories and current category string.
+- Request Arguments: `page` - integer
+- Returns: An object with 10 paginated questions, total questions, object including all categories, and current category string
+  If no page provided, it returns all the questions in the database
+
+Example: `curl -H "Content-Type: application/json" http://127.0.0.1:5000/questions?page=1`
+
+```
+{
+  "questions": [
+    {
+      "id": 1,
+      "question": "This is a question",
+      "answer": "This is an answer",
+      "difficulty": 5,
+      "category": 2
+    }
+  ],
+  "totalQuestions": 100,
+  "categories": {
+    "1": "Science",
+    "2": "Art",
+    "3": "Geography",
+    "4": "History",
+    "5": "Entertainment",
+    "6": "Sports"
+  },
+  "currentCategory": "History"
+}
+```
+
+#### `GET '/categories/${id}/questions'`
+
+- Fetches questions for a cateogry specified by id request argument
+- Request Arguments: id - integer
+- Returns: An object with questions for the specified category, total questions, and current category string
+
+Example: `curl -H "Content-Type: application/json" http://127.0.0.1:5000/categories/1/questions`
+
+```
+{
+  "questions": [
+    {
+      "id": 1,
+      "question": "This is a question",
+      "answer": "This is an answer",
+      "difficulty": 5,
+      "category": 4
+    }
+  ],
+  "totalQuestions": 100,
+  "currentCategory": "History"
+}
+```
+
+#### `DELETE '/questions/${id}'`
+
+- Deletes a specified question using the id of the question
+- Request Arguments: id - integer
+- Returns: Does not need to return anything besides the appropriate HTTP status code. Optionally can return the id of the question. If you are able to modify the frontend, you can have it remove the question using the id instead of refetching the questions.
+
+Example: `curl -X DELETE -H "Content-Type: application/json" http://127.0.0.1:5000/questions/1`
+
+```
+{
+  "deleted_id": 1,
+}
+```
+
+#### `POST '/quizzes'`
+
+Sends a post request in order to get the next question
+
+Example: `curl -X POST -d "{\"previous_questions\": [1, 4, 20, 15], \"quiz_category\": \"Art\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/quizzes`
+
+```
+{
+  "question": {
+    "id": 1,
+    "question": "This is a question",
+    "answer": "This is an answer",
+    "difficulty": 5,
+    "category": 4
+  }
+}
+or
+{}
+```
+
+#### `POST '/questions'`
+
+Sends a post request in order to add a new question
+
+Example: `curl http://127.0.0.1:5000/questions -X POST -H "Content-Type: application/json" -d '{"question": "Heres a new question string","answer": "Heres a new answer string","difficulty": 1,"category": 3}'`
+
+```
+{
+  "questions": [
+    {
+      "id": 1,
+      "question": "This is a question",
+      "answer": "This is an answer",
+      "difficulty": 5,
+      "category": 5
+    }
+  ],
+  "totalQuestions": 100,
+  "currentCategory": "Entertainment"
+}
+```
+
+#### `POST '/questions'`
+
+Sends a post request in order to search for a specific question by search term
+Example: `{'searchTerm': 'this is the term the user is looking for'}`
+
+```
+{
+  "questions": [
+    {
+      "id": 1,
+      "question": "This is a question",
+      "answer": "This is an answer",
+      "difficulty": 5,
+      "category": 5
+    }
+  ],
+  "totalQuestions": 100,
+  "currentCategory": "Entertainment"
+}
+```
+
+## Author(s)
+
+- [Duc HoangMinh](https://github.com/itsnot-aduck)
+- Powered by Udacity
+
+## Acknowledgements
+
+I want to say thank to the team at Udacity for great lessions, also a great gratitude for StackOverFlow :D
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,22 +6,70 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+database_name = "trivia_test"
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
+    # def initializeDatabase():
+    #     """Build test database to execute test cases"""
+    #     #delete existing test database
+    #     bashCommand = "dropdb {}".format(database_name)
+    #     os.system(bashCommand)
+    #     #setup new test database
+    #     bashCommand = "createdb {}".format(database_name)
+    #     os.system(bashCommand)
+    #     #load test database with example data
+    #     bashCommand = "psql {} < {}.psql".format(database_name, database_name)
+    #     os.system(bashCommand)
+
     def setUp(self):
         """Define test variables and initialize app."""
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        
-        self.app = create_app({
-            "SQLALCHEMY_DATABASE_URI": self.database_path
-        })
-
+        self.app = create_app()
         self.client = self.app.test_client
+        self.database_name = database_name
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
 
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+        
+        # input to submit a new question (positive test)
+        self.add_question = {
+            "question": "Whats is Caryn McCarthy's favorite author?",
+            "answer": "Neill Gaiman",
+            "difficulty": 1,
+            "category": 2
+        }
+        # failure to submit a new question (negative test)
+        self.fail_add_question = {
+            # 'difficulty' is missing!
+            "question": "What planet is closed to the sun?",
+            "answer": "Mercury",
+            "category": 1
+        }
+
+        #input for play game (positive test)
+        self.play_quiz = {
+            "quiz_category":5,
+            "previous_questions":[5,]
+        }
     
+        #error for input to play game (negative test)
+        self.error_play_quiz = {
+            "quiz_category":"Entertainment",
+            "previous_questions":[5,]
+        }
+
+        self.wrongdata_play_quiz = {
+            "field01":"Something not existing",
+            "field02":"Something else not existing"
+        }
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -31,6 +79,200 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_categories(self):
+        """ Test GET /categories """
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['categories'])
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_categories'])
+
+    # Test warning message if no categories are maintained in DB
+    # def test_404_no_categories(self):
+    #     """ Test abort for no categories """
+    #     res = self.client().get('/categories')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 404)
+    #     self.assertEqual(data['success'], False)
+    #     self.assertTrue(data['message'], 'resource not found')
+
+    def test_get_category_id(self):
+        """ Test not supported GET method /questions """
+        response = self.client().get('/categories/1')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['message'], 'resource not found')
+        self.assertEqual(data['success'], False)
+
+
+    def test_get_questions(self):
+        """ Test GET /questions """
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data['questions'])
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+
+    def test_get_question_id(self):
+        """ Test not supported GET method /questions """
+        response = self.client().get('/questions/5')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(data['message'], 'method not allowed for endpoint')
+        self.assertEqual(data['success'], False)
+
+    def test_get_paginated_questions(self):
+        result = self.client().get('/questions?page=1')
+        data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["categories"]))
+        self.assertTrue(len(data["questions"]))
+
+    def test_404_get_paginated_questions_beyond_valid_page(self):
+        result = self.client().get('/questions?page=9999')
+        data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_add_question(self):
+        """ Test adding new question to database """
+        # count number of questions in initial db
+        initial_questions = len(Question.query.all())
+        # post new question to db
+        response = self.client().post("/questions", json=self.add_question)
+        data = json.loads(response.data)
+        # count new number of questions in db
+        new_questions = len(Question.query.all())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(new_questions - initial_questions == 1)
+
+    def test_search_questions(self):
+        """ return result for search term strings """
+        response = self.client().post("/questions",
+            json={"searchTerm": "title"})
+
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data["questions"]), 2)
+    
+    def test_delete_question(self):
+        """ Test DELETE method for '/questions' using unique question ID """
+        response = self.client().delete('/questions/20')
+        data = json.loads(response.data)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data["total_questions"])
+
+    def test_404_delete_question(self):
+        result = self.client().delete('/questions/9999')
+        data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_invalid_questions_page(self):
+        """Test GET invalid questions page """
+        response = self.client().get('/questions?page=1000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    def test_get_questions_category(self):
+        """ Test GET questions for given category """
+        response = self.client().get("/categories/2/questions")
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+        
+    def test_get_questions_category_error(self):
+        """ Test GET questions for not existing category """
+        response = self.client().get("/categories/99/questions")
+
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+
+    def test_add_question(self):
+        """ Test POST new question """
+        initial_questions = len(Question.query.all())
+        response = self.client().post("/questions", json=self.add_question)
+        data = json.loads(response.data)
+        new_questions = len(Question.query.all())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(new_questions - initial_questions == 1)
+
+    def test_422_post_new_question(self):
+        response = self.client().post("/questions", json=self.fail_add_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "data is unprocessable")
+
+    def test_get_paginated_question_by_category(self):
+        response = self.client().get('/categories/1/questions?page=1')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["categories"]))
+        self.assertTrue(len(data["questions"]))
+
+    def test_404_get_paginated_question_by_category_beyond_valid_page(self):
+        response = self.client().get('/categories/1/questions?page=200')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_play_game(self):
+        response = self.client().post("/quizzes", json=self.play_quiz)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+
+    def test_404_play_game(self):
+        response = self.client().post("/quizzes", json=self.error_play_quiz)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_422_play_game(self):
+        response = self.client().post('/quizzes', json=self.wrongdata_play_quiz)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
